dc2a2df165229460c63e43024f6f797d9d719a39
hasdajustin <matheusjustinhasda@gmail.com>
2025-08-02 04:22:10 +0600

    add blog list API endpoint using APIView

--- backend/myBlog/views.py
+++ backend/myBlog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Blog
+from .serializers import BlogSerializer
 
 # Create your views here.
+class BlogList(APIView):
+    def get(self):
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(blogs, many=True)
+        return Response(serializer.data)
