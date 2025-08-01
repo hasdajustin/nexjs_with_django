1f3296b95b92c0efb5d4ba0396155977d4c366cf
hasdajustin <matheusjustinhasda@gmail.com>
2025-08-02 03:54:08 +0600

    create Blog model

--- backend/myBlog/models.py
+++ backend/myBlog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Blog(models.Model):
+    image = models.ImageField(upload_to='blog_images/')
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
